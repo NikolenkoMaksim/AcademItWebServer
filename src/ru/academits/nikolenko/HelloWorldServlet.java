adac507c0ac398d34e945b519f14886a94e397ec
NikolenkoMaksim <kinik33@mail.ru>
2021-07-05 21:51:21 +0700

    Done HelloWorldServlet for Tomcat9 server

--- src/ru/academits/nikolenko/HelloWorldServlet.java
+++ src/ru/academits/nikolenko/HelloWorldServlet.java
@@ -5,14 +5,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serial;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class HelloWorldServlet extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 6056418773018318646L;
 
     protected void doGet(HttpServletRequest reg, HttpServletResponse resp) throws IOException {
-        resp.getOutputStream().write("Hello from HelloWorldServlet".getBytes(Charset.forName("UTF-8")));
+        resp.getOutputStream().write("Hello from HelloWorldServlet. Method: GET".getBytes(StandardCharsets.UTF_8));
+        resp.getOutputStream().flush();
+        resp.getOutputStream().close();
+    }
+
+    protected void doPost(HttpServletRequest reg, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().write("Hello from HelloWorldServlet. Method: POST".getBytes(StandardCharsets.UTF_8));
         resp.getOutputStream().flush();
         resp.getOutputStream().close();
     }
